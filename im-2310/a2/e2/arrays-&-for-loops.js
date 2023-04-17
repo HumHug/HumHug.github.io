0a3aa24e76d257252ca8239f4b89f02a7c032371
HumHug <56522462+HumHug@users.noreply.github.com>
2023-04-17 15:19:24 +1000

    Add files via upload

---
+++ im-2310/a2/e2/arrays-&-for-loops.js
@@ -0,0 +1,28 @@
+var fruit1 = "banana";
+var fruit2 = "pineapple";
+var fruit3 = "apple";
+var fruit4 = "peach";
+var fruit5 = "orange";
+
+var fruitArray = ["banana", "pineapple", "apple", "peach", "orange"];
+
+var mixedArray = [42, "pineapple", true, [3, 6, "nine"], "orange"];
+
+console.log("I'm going to help test the array");
+
+
+
+
+console.log(fruitArray)
+
+for(let fruit of fruitArray)
+{
+    if(fruit === "apple")
+    {
+        console.log("apple i am!")
+    } 
+    else
+    {
+        console.log(fruit);
+    }
+}
